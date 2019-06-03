678bf9dce9382c01fac5c960bdbaee3a318c1874
Arief R <41944717+wayangcode@users.noreply.github.com>
2019-06-03 12:59:29 +0700

    Update about.bash

--- lib/about.bash
+++ lib/about.bash
@@ -4,7 +4,7 @@ echo -e "${GREEN}[-]${CYAN} Description :"
 echo -e "${YELLOW}    This tools was created for download photos,
     videos, profile picture on Instagram without 
     authentication."
-echo -e "${GREEN}[-]${CYAN} Version :${YELLOW} 0.1.0"
+echo -e "${GREEN}[-]${CYAN} Version :${YELLOW} 0.1.5"
 echo -e "${GREEN}[-]${CYAN} Author  :${YELLOW} https://github.com/wayangcode (Arief R)\n"
 echo -ne "${Y1}[c]${CYAN}ontinue/${P1}[e]${CYAN}xit : "
 read option
