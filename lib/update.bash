43f1c81a126de9a6c94a3851931e1b373a3d77a2
Arief R <41944717+wayangcode@users.noreply.github.com>
2019-06-03 12:53:27 +0700

    Update update.bash

--- lib/update.bash
+++ lib/update.bash
@@ -2,9 +2,43 @@
 
 update() {
     local dir=$(echo "$(pwd)/..")
-    echo -e "${GREEN}[-] ${CYAN}Cloning repository ...${YELLOW}"
-    git clone https://github.com/wayangcode/instake.git "${pwd}/instake"
-    echo -e "${GREEN}[-] ${CYAN}Instake updated\n"
+    echo -e "${GREEN}[-] ${CYAN}Check lib ..."
+    
+    local photo="https://raw.githubusercontent.com/wayangcode/instake/master/lib/photos.bash"
+    local video="https://raw.githubusercontent.com/wayangcode/instake/master/lib/videos.bash"
+    local propic="https://raw.githubusercontent.com/wayangcode/instake/master/lib/profile_picture.bash"
+    local about="https://raw.githubusercontent.com/wayangcode/instake/master/lib/about.bash"
+    local update="https://raw.githubusercontent.com/wayangcode/instake/master/lib/update.bash"
+    
+    # PHOTOS SECTION
+    local photos=$(curl -s "$photo" | md5sum | awk '{print $1}')
+    if [[ ! $photos == $(md5sum lib/photos.bash | awk '{print $1}') ]]; then
+        curl -s "$photo" -o lib/photos.bash
+        echo -e "${YELLOW}[+]${GREEN} photos.bash updated"
+    fi
+    
+    # VIDEOS SECTION
+    local videos=$(curl -s "$video" | md5sum | awk '{print $1}')
+    if [[ ! $videos == $(md5sum lib/videos.bash | awk '{print $1}') ]]; then
+        curl -s "$video" -o lib/videos.bash
+        echo -e "${YELLOW}[+]${GREEN} video.bash updated"
+    fi
+    
+    # PROFILE_PIC SECTION
+    local profile_pic=$(curl -s "$propic" | md5sum | awk '{print $1}')
+    if [[ ! $videos == $(md5sum lib/videos.bash | awk '{print $1}') ]]; then
+        curl -s "$propic" -o lib/videos.bash
+        echo -e "${YELLOW}[+]${GREEN} profile_picture.bash updated"
+    fi
+    
+    # UPDATE SECTION
+    local uptodate=$(curl -s "$update" | md5sum | awk '{print $1}')
+    if [[ ! $uptodate == $(md5sum lib/videos.bash | awk '{print $1}') ]]; then
+        curl -s "$update" -o lib/update.bash
+        echo -e "${YELLOW}[+]${GREEN} update.bash updated"
+    fi
+    
+    echo -e "${GREEN}[~] ${CYAN}DONE\n"
     echo -ne "${Y1}[c]${CYAN}ontinue/${P1}[e]${CYAN}xit : "
     read option
     case $option in
